f49402bd1f7eb2801f79aa267f6dcded6d4b01de
lrodenyoder <rodenlau000@gmail.com>
2022-05-25 15:53:41 -0500

    set up resolver mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { User, Restaurant } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 //TODO: uncomment when authorization is set up
-// const { AuthenticationError } = require("apollo-server-express");
 // const { signToken } = require("../utils/auth");
 
 const resolvers = {
@@ -23,19 +23,45 @@ const resolvers = {
         .populate("restaurants");
     },
     users: async () => {
-        return User.find()
-            .select("-__v -password")
-            .populate("restaurants");
+      return User.find().select("-__v -password").populate("restaurants");
     },
     restaurant: async (parent, { _id }) => {
-        return Restaurant.findOne({ _id });
+      return Restaurant.findOne({ _id });
     },
-    restaurants: async (parent, {username }) => {
-        //optional search by username
-        const params = username ? { username } : {};
+    restaurants: async (parent, { username }) => {
+      //optional search by username
+      const params = username ? { username } : {};
 
-        //sort in descending order by createdAt
-        return Restaurant.find(params).sort({ createdAt: -1 });
+      //sort in descending order by createdAt
+      return Restaurant.find(params).sort({ createdAt: -1 });
+    },
+  },
+  Mutation: {
+    addUser: async (parent, {args}) => {
+      const user = await User.create(args);
+      //TODO: uncomment when authentication is set up
+      // const token = signToken(user);
+
+      return { user };
+      //TODO: uncomment below and delete above when authentication is set up
+      // return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError("Incorrect login credentials");
+        }
+
+        const correctPW = await user.isCorrectPassword(password); //TODO: add method in User model
+        if (!correctPW) {
+            throw new AuthenticationError("Incorrect login credentials");
+
+        }
+
+        // const token = signToken(user);
+        // return {token, user};
+        return {user};
     }
   },
 };
