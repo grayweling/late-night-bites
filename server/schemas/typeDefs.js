ffc66b871bd3ed656244970b1e2175562baa1111
lrodenyoder <rodenlau000@gmail.com>
2022-05-25 14:44:44 -0500

    add beginning typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,38 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    restaurants: [Restaurant]
+  }
+  type Restaurant {
+    _id: ID
+    restaurantText: String
+    createdAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+  type Query {
+      users: [User]
+      user(username: String!): User
+      restaurants(username: String): [Restaurant]
+      restaurant(_id: ID!): Restaurant
+  }
+  type Mutation {
+      login(email: String!, password: String!): User
+      addUser(username: String!, email: String!, password: String!): User
+      addRestaurant(restaurantText: String!): Restaurant
+      addComment(restaurantId: ID!, commentBody: String): Restaurant
+  }
 `;
 
 module.exports = typeDefs;
