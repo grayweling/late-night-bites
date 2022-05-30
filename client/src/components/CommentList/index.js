a1403ee7cf50df0d26759da7676508ad8b6a956e
lrodenyoder <rodenlau000@gmail.com>
2022-05-30 17:46:04 -0500

    add commentList component

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -5,9 +5,15 @@ const CommentList = ({ comments }) => {
     return (
                 <div className="text-center p-6">
                   <h3 className="description text-[#BE95FA] mb-4">
-                    click for cmments
-                  </h3>
-                </div>
+                    Map comment list here
+            </h3>
+            {comments && comments.map((comment) => (
+                <p className="pill mb-3" key={comment._id}>
+                    {comment.commentBody} {"// "}
+                    {comment.username} on {comment.createdAt}
+                </p>
+            ))}
+            </div>
     )
 }
 
