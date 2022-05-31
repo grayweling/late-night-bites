7a8950082fa80c377dd425a4a3098e9d9249affe
David <davidkramm53@gmail.com>
2022-05-31 14:25:45 -0500

    made a few spacing changes on createpost page

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -59,7 +59,7 @@ const CreatePost = () => {
                         </span>
 
                         <form method="post" action="/create" encType="multipart/form-data" onSubmit={handleFormSubmit}>
-                            <input type="file" id="post-images" name="image" className="mb-8 mt-32" />
+                            <input type="file" id="post-images" name="image" className="mb-8 mt-20" />
 
                             <h3 className="text-center">Name</h3>
                             <input name="name" id="Name" type="text"  onChange={handleChange} value={userFormData.name} placeholder="Enter the name of the place" className="rounded-full text-center w-full mb-8 text-black" />
@@ -93,7 +93,7 @@ const CreatePost = () => {
 
 
                             </select>
-                            <button type="submit" className="inline-block px-6 py-2.5 bg-[#BE95FA] text-white font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-[#16AC97] hover:shadow-lg focus:bg-[#16AC97] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#16AC97] active:shadow-lg transition duration-150 ease-in-out">
+                            <button type="submit" className="mb-4 inline-block px-6 py-2.5 bg-[#BE95FA] text-white font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-[#16AC97] hover:shadow-lg focus:bg-[#16AC97] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#16AC97] active:shadow-lg transition duration-150 ease-in-out">
 
                                 <i className="fa-brands fa-creative-commons-share fa-lg"></i>
                                 <h3>Click to Share</h3>
