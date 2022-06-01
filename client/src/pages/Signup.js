de7f3b4f17201c0d74589afac7641f583a031e37
David <davidkramm53@gmail.com>
2022-05-31 19:42:55 -0500

    changed username placeholder

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -67,7 +67,7 @@ const Signup = () => {
                                             type="username"
                                             name="username"
                                             id="username"
-                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@mail.com"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="username"
                                             required=""
                                             value={formState.username}
                                             onChange={handleChange} />
